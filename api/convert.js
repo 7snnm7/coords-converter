e731495d1467e0306c054ec25fcfc9df9052b0cb
7snnm7 <7snnm7@gmail.com>
2025-10-27 01:23:39 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,24 +1,8 @@
 function convert(q) {
-  const mapping = {
-    q: "ض", w: "ص", e: "ث", r: "ق", t: "ف", y: "غ", u: "ع", i: "ه", o: "خ", p: "ح",
-    "[": "ج", "]": "د", a: "ش", s: "س", d: "ي", f: "ب", g: "ل", h: "ا", j: "ت",
-    k: "ن", l: "م", ";": "ك", "'": "ط", z: "ئ", x: "ء", c: "ؤ", v: "ر", b: "لا",
-    n: "ى", m: "ة", ",": "و", ".": "ز", "/": "ظ"
-  };
-
-  const hasArabic = /[\u0600-\u06FF]/.test(q);
-
-  if (hasArabic) {
-    return "Input should be in English, so I’ll shoutout my creator, check out twitch.tv/HassanNM7 :D";
-  }
-
-  const converted = q
-    .split("")
-    .map((char) => mapping[char.toLowerCase()] || char)
-    .join("");
-
-  return converted;
+    let nums = q.trim().split(" ").map(Number);
+    if (nums.length < 2 || nums.some(isNaN)) return "Input must be numbers";
+    let x = Math.round(nums[0] / 8);
+    let z = Math.round(nums[nums.length - 1] / 8);
+    return `(${x}, Y, ${z})`;
 }
-
-var result = convert(q);
-result;
+convert(q);
