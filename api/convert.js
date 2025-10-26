ffc03738ae64a36888fbd9e92e58b41a6c354e1a
7snnm7 <7snnm7@gmail.com>
2025-10-27 01:33:45 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,8 +1,22 @@
 function convert(q) {
-    let nums = q.trim().split(" ").map(Number);
-    if (nums.length < 2 || nums.some(isNaN)) return "Input must be numbers";
-    let x = Math.round(nums[0] / 8);
-    let z = Math.round(nums[nums.length - 1] / 8);
-    return `(${x}, Y, ${z})`;
+  if (!q) return 'Input should be in this state “!c OX OY OZ” or “!c NX NY NZ” check out my creator twitch.tv/hassannm7';
+  
+  const parts = q.trim().split(/\s+/).filter(Boolean);
+  const isOver = parts[0].toLowerCase() === 'o';
+  const nums = parts.filter(x => !isNaN(x)).map(Number);
+
+  if (nums.length < 2 || nums.length > 3)
+    return 'Input should be in this state “!c OX OY OZ” or “!c NX NY NZ” check out my creator twitch.tv/hassannm7';
+
+  if (isOver) {
+    const x = Math.round(nums[0] * 8);
+    const z = Math.round(nums[nums.length - 1] * 8);
+    return `Overworld Side Coordinates Are: (${x}, Y, ${z})`;
+  } else {
+    const x = Math.round(nums[0] / 8);
+    const z = Math.round(nums[nums.length - 1] / 8);
+    return `Nether Side Coordinates Are: (${x}, Y, ${z})`;
+  }
 }
+
 convert(q);
